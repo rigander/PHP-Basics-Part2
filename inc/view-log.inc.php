a0e996328854c1c9749ab250d7df88441911fb6d
Andrey Komov <andrewkomov@gmail.com>
2022-08-25 07:20:09 +0300

    log2

--- inc/view-log.inc.php
+++ inc/view-log.inc.php
@@ -1,4 +1,21 @@
 <?php
+//Var1
+if(is_file("log/" .PATH_LOG)):
+    $file = file("log/".PATH_LOG);
+    echo "<ol>";
+    foreach ($file as $line):
+      list($dts, $page, $ref) = explode("|", $line);
+      $dts = date("d-m-Y", (int)$dts);
+      echo "<li>";
+      echo "$dts - $ref -> $page";
+      echo "</li>";
+    endforeach;
+    echo "</ol>";
+endif;
+
+
+
+//Var2
 $file = fopen("log/path.log", "r");
 $lines = [ ];
 $x = 1;
