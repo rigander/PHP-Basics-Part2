0847a5e737813433ee596b23001445e7b6bd37bf
Andrey Komov <andrewkomov@gmail.com>
2022-08-25 06:03:49 +0300

    log

--- inc/log.inc.php
+++ inc/log.inc.php
@@ -1,7 +1,8 @@
 <?php
 $dt = date("F j, Y, g:i a");
-$page = $_SERVER["REQUEST_URI"];
+$page = $_GET["id"] ?? "index";
 $ref = $_SERVER["HTTP_REFERER"];
+$ref = pathinfo($ref, PATHINFO_BASENAME);
 $path = "$dt|$page|$ref\r\n";
 file_put_contents("log/" . PATH_LOG, "$path", FILE_APPEND);
 ?>
