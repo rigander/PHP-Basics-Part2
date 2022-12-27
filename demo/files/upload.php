ee541db48559099a1b835f4efdad35eafe40dafc
Andrey Komov <andrewkomov@gmail.com>
2022-12-27 12:04:25 +0200

    manipulation with sql server

--- demo/files/upload.php
+++ demo/files/upload.php
@@ -4,7 +4,7 @@
 	if($_SERVER['REQUEST_METHOD']=='POST'){
 		var_dump($_FILES);
         $n = $_FILES['userfile']['name'];
-        $t = $_FILES['userfile']['tmp_name'];
+        $t = $_FILES['userfileclea']['tmp_name'];
         move_uploaded_file($t, $n);
 	}
 ?>
