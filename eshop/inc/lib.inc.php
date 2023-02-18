fd07a70aba08efa97ac2aecb5a4d18f40065b7f8
Andrey Komov <andrewkomov@gmail.com>
2023-02-18 20:36:43 +0200

    eshop_p.8

--- eshop/inc/lib.inc.php
+++ eshop/inc/lib.inc.php
@@ -133,6 +133,33 @@ function deleteItemFromBasket($id){
     saveBasket();
 }
 
+function clearBasket(){
+    // todo таким образом куки basket будет удален,
+    //  ведь выставлено отрицательное значение времени.
+    setcookie('basket', 'deleted', time()-3600);
+}
+
+function saveOrder($datetime){
+    global $link, $basket; $goods = myBasket();
+    // todo - mysqli_stmt_init — Инициализирует запрос и возвращает объект
+    //   для использования в mysqli_stmt_prepare.
+    $stmt = mysqli_stmt_init($link);
+    $sql = 'INSERT INTO orders (
+            title, author, pubyear, price, quantity, orderid, datetime) 
+            VALUES (?, ?, ?, ?, ?, ?, ?)';
+    // todo mysqli_stmt_prepare — Подготавливает утверждение SQL к выполнению.
+    if (!mysqli_stmt_prepare($stmt, $sql)) return false;
+    // todo - _bind_param Связывает переменную PHP с параметром SQL-выражения.
+    foreach($goods as $item){ mysqli_stmt_bind_param($stmt, "ssiiisi",
+        $item['title'], $item['author'],
+        $item['pubyear'], $item['price'],
+        $item['quantity'], $basket['orderid'], $datetime);
+    // todo mysqli_stmt_execute — Выполняет подготовленное утверждение.
+        mysqli_stmt_execute($stmt); }
+    mysqli_stmt_close($stmt);
+    clearBasket();
+    return true;
+}
 
 function debugArray($array){
     $log = date('Y-m-d H:i:s') . ' ';
--- eshop/saveorder.php
+++ eshop/saveorder.php
@@ -1,12 +1,33 @@
 <?php
 	require "inc/lib.inc.php";
 	require "inc/config.inc.php";
+
+    //todo Получаем данные из массива POST.
+    $name = clearStr($_POST["name"]);
+    $email = clearStr($_POST["email"]);
+    $phone = clearStr($_POST["phone"]);
+    $address = clearStr($_POST["address"]);
+
+    global $basket;
+    // todo Получаем идентификатор заказа oid.
+    $oid = $basket["orderid"];
+    $dt = time();
+    $order = "$name|$email|$phone|$address|$oid|$dt\n";
+    //todo file_put_contents — Пишет данные в файл. В этой функции
+    //  доступны некоторые флаги:
+    //  FILE_APPEND - Если файл filename уже существует,
+    //  данные будут дописаны в конец файла вместо того, чтобы его перезаписать.
+    //  ORDERS_LOG - имя файла, в который будут записаны данные.
+    file_put_contents("admin/".ORDERS_LOG, $order, FILE_APPEND);
+    saveOrder($dt);
+
 ?>
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="utf-8">
 	<title>Сохранение данных заказа</title>
+    <link rel="stylesheet" type="text/css" href="../../inc/style.css" />
 </head>
 <body>
 	<p>Ваш заказ принят.</p>
